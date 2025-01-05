d4376db74d2486d8358d2a4faa88fe1e91629162
Satya Kodamanchili <kssasarma@gmail.com>
2025-01-05 10:42:50 +0530

    Cleaner code

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # tic-tac-toe-project
-A simple tic-tac-toe project to understand advanced react concepts
+A simple tic-tac-toe project to understand advanced react concepts.
--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,17 +1,4 @@
-// import { useState } from "react";
-
 function GameBoard({ onSelectSquare, board /*activePlayerSymbol*/ }) {
-	// const [gameBoard, setGameBoard] = useState(initialGameBoard);
-
-	// function handlSelectSquare(rowIndex, colIndex) {
-	// 	setGameBoard((previousGameBoard) => {
-	// 		const updatedBoard = [...previousGameBoard.map((innerArry) => [...innerArry])];
-	// 		updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-	// 		return updatedBoard;
-	// 	});
-	// 	onSelectSquare();
-	// }
-
 	return (
 		<ol id="game-board">
 			{board.map((row, rowIndex) => (
