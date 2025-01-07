51437395edcd4945288c989d2de6b746831b63dc
Satya Kodamanchili <kssasarma@gmail.com>
2025-01-07 12:23:27 +0530

    vite config and .env files comitted

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+	// Load environment variables to use as base path
+	const env = loadEnv(mode, process.cwd());
+	const BASE_PATH = env.VITE_BASE_PATH;
+
+	return {
+		plugins: [react()],
+		base: BASE_PATH,
+	};
+});
